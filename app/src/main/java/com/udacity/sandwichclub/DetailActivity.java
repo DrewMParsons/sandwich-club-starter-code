73723f9a0b47928dda9ed1291e376aa6639b2311
Drew Parsons <drewmparsons@gmail.com>
2020-03-28 11:01:34 -0400

    JsonUtils remove hardcoded strings, add optString() Activity_detail: move image inside scrollview DetailActivity: check if data is null or unavailable, display "NO DATA FOUND" AndroidManifest: add parent Activity to detail activity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,24 +3,31 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
+import com.udacity.sandwichclub.databinding.ActivityDetailBinding;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
+
+import java.util.List;
 
 public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_POSITION = "extra_position";
+    public static final String NO_DATA_FOUND = "No Data Found";
     private static final int DEFAULT_POSITION = -1;
+    private ActivityDetailBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_detail);
+        binding = ActivityDetailBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
 
         Intent intent = getIntent();
         if (intent == null) {
@@ -43,10 +50,10 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
-                .into(ingredientsIv);
+                .into(binding.imageIv);
 
         setTitle(sandwich.getMainName());
     }
@@ -56,7 +63,40 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        binding.alsoKnownTv.setText(listToString(sandwich.getAlsoKnownAs()));
+        binding.originTv.setText(nullCheck(sandwich.getPlaceOfOrigin()));
+        binding.ingredientsTv.setText(listToString(sandwich.getIngredients()));
+        binding.descriptionTv.setText(nullCheck(sandwich.getDescription()));
+
+
+    }
+    /*
+    * Method used to check if Strings are null
+    * if NUll: returns a String value of "No data Found"
+    * if not NULL: returns original value*/
+
+    private String nullCheck(String s){
+        if(s.isEmpty() || s ==null){
+            return NO_DATA_FOUND;
+        }
+        return s;
+    }
+    /*
+     * Method used to check if Lists of Strings are null
+     * if NUll: returns a String value of "No data Found"
+     * if not NULL: returns original values as a string deliminated by ','*/
+
+    private String listToString(List<String> list){
+        StringBuilder sb = new StringBuilder();
+        if(list.isEmpty()){
+            return NO_DATA_FOUND;
+        }else{
+            for (String s: list){
+                sb.append(s).append(", ");
+            }
+            return sb.substring(0, sb.length() - 2);
+        }
 
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,52 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+
 public class JsonUtils {
+    public static final String NAME = "name";
+    public static final String MAIN_NAME = "mainName";
+    public static final String AKA = "alsoKnownAs";
+    public static final String ORIGIN = "placeOfOrigin";
+    public static final String DESCRIPTION = "description";
+    public static final String IMAGE = "image";
+    public static final String INGREDIENTS = "ingredients";
+
+
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            JSONObject sandwichJSON = new JSONObject(json);
+            JSONObject name = sandwichJSON.getJSONObject(NAME);
+            String mainName = name.optString(MAIN_NAME);
+            JSONArray aka = name.getJSONArray(AKA);
+            ArrayList<String> akaList = createListFromJSON(aka);
+            String placeOfOrigin = sandwichJSON.optString(ORIGIN);
+            String description = sandwichJSON.optString(DESCRIPTION);
+            String image = sandwichJSON.optString(IMAGE);
+            JSONArray ing = sandwichJSON.getJSONArray(INGREDIENTS);
+            ArrayList<String>ingredients = createListFromJSON(ing);
+            return new Sandwich(mainName,akaList,placeOfOrigin,description,image,ingredients);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static ArrayList<String> createListFromJSON(JSONArray aka) throws JSONException
+    {
+        ArrayList<String> list= new ArrayList<>();
+        if(aka !=null){
+            int len = aka.length();
+            for (int i = 0; i < len; i++) {
+                list.add(aka.get(i).toString());
+            }
+        }
+        return list;
     }
 }
